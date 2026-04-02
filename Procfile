ba196ca20cd8b9d51ff77408afd284b9be788999
AliHHasan1 <eng.alihasan2@gmail.com>
2026-04-03 00:26:50 +0300

    Fix: Add collectstatic to Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn wazifni_backend.wsgi
+web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn wazifni_backend.wsgi
--- wazifni_backend/settings.py
+++ wazifni_backend/settings.py
@@ -150,7 +150,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
