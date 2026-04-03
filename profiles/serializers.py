1774190e041757a6d317c047cc8de37d62ce92e8
AliHHasan1 <eng.alihasan2@gmail.com>
2026-04-03 16:22:20 +0300

    git commit -m "Fix: update profile's serializers.py 6"

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -70,27 +70,24 @@ class ProfileSerializer(serializers.ModelSerializer):
         existing_items = {item.id: item for item in related_manager.all()}
         incoming_ids = []
 
-        def update_nested_field(self, instance, field_name, serializer_class, data):
-            related_manager = getattr(instance, field_name)
-            existing_items = {item.id: item for item in related_manager.all()}
 
-            for item_data in data:
-                item_id = item_data.get('id')
-                context = {'profile': instance}
+        for item_data in data:
+            item_id = item_data.get('id')
+            context = {'profile': instance}
 
-                if item_id and item_id in existing_items:
-                    # ✅ update
-                    item_instance = existing_items[item_id]
-                    serializer = serializer_class(item_instance,data=item_data,partial=True,context=context)
-                    serializer.is_valid(raise_exception=True)
-                    serializer.save()
+            if item_id and item_id in existing_items:
+                # ✅ update
+                item_instance = existing_items[item_id]
+                serializer = serializer_class(item_instance,data=item_data,partial=True,context=context)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
 
-                else:
-                    # ✅ create
-                    item_data.pop('id', None)
-                    serializer = serializer_class(data=item_data, context=context)
-                    serializer.is_valid(raise_exception=True)
-                    serializer.save() # ربط مباشر بالبروفايل الحالي
+            else:
+                # ✅ create
+                item_data.pop('id', None)
+                serializer = serializer_class(data=item_data, context=context)
+                serializer.is_valid(raise_exception=True)
+                serializer.save() # ربط مباشر بالبروفايل الحالي
 
     def update(self, instance, validated_data):
         nested_fields_map = {
