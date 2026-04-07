2f56d9ad506d6e11bf582ac5aa09530547097768
AliHHasan1 <eng.alihasan2@gmail.com>
2026-04-07 11:32:16 +0300

    git commit -m "add django-extensions"

--- wazifni_backend/settings.py
+++ wazifni_backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
+    'django_extensions',
     'rest_framework',
     'rest_framework_simplejwt', # Added for JWT
     'corsheaders',
